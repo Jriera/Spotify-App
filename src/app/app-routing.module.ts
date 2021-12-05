0ce6c17dc103f43cd9c18080ef3c083ca2e89e8a
Jriera <jordiriera81@gmail.com>
2021-12-05 18:07:42 +0100

    chore:update routing for login and welcome components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CallbackComponent } from './callback/callback.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
-  {path: 'callback', component: CallbackComponent},
-  {path:'welcome', component: WelcomeComponent},
-  {path: '', component: CallbackComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path:'login', component: LoginComponent},
+  {path: '', component: WelcomeComponent},
+  
 ];
 
 @NgModule({
