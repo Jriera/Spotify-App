07a9d1a214fd1bd5f8f3e60cd6821e133b981c8c
Jriera <jordiriera81@gmail.com>
2021-12-05 17:57:05 +0100

    feat:Create login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '../services/http.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  code:string|null = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+}
