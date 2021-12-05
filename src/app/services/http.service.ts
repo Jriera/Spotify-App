3eab9173c6a369ced01735c10d0929a23f66433b
Jriera <jordiriera81@gmail.com>
2021-12-05 18:04:39 +0100

    fix:update redirect url

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment.prod';
+import { TokenResponse } from '../token-response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class HttpService {
   
     let clientId = environment.clientId; // Your client id
     let clientSecret = environment.clientSecret; // Your secret
-    const redirect_uri = 'http://localhost:4200/callback'; // Your redirect uri
+    const redirect_uri = 'http://localhost:4200/welcome'; // Your redirect uri
     let apiURL = "https://accounts.spotify.com/api/token";
   
     const encodedClient = btoa(`${clientId}:${clientSecret}`);
@@ -24,9 +25,6 @@ const headers = new HttpHeaders({
   
     let params = new HttpParams();
     params = params.append('grant_type', 'client_credentials');
-    /* params = params.append('code', 'AQCcKUYqn2fqK240h1xhMk7o8L1rPts7khDRSlmaAWTvjS2hR0U_SNkFwBkd0DWd0iBwwnW2PpMekRrYHmjA9Annt5to5tWbqxHE4AjRewS2hiFC0wF6PriIyGzJdNspyrDbeEigeapPBhQO-WOm-9c7FEj9c-KU7cSt7W8Za8GqpuNfeF-CRnv41oFbVip7M9d0fVukhTU0CdMpkaNWwQfI6ZzFMA');
-    params = params.append('redirect_uri', redirect_uri); */
-  
     return this.http.post(apiURL,params, { headers })
     
   }
@@ -34,7 +32,7 @@ const headers = new HttpHeaders({
   getToken(code:string) {
     let clientId = environment.clientId; // Your client id
     let clientSecret = environment.clientSecret; // Your secret
-    const redirect_uri = 'http://localhost:4200/callback'; // Your redirect uri
+    const redirect_uri = 'http://localhost:4200/welcome'; // Your redirect uri
     let apiURL = environment.tokenUrl;
   
     const encodedClient = btoa(`${clientId}:${clientSecret}`);
@@ -49,7 +47,7 @@ const headers = new HttpHeaders({
     params = params.append('code', code);
     params = params.append('redirect_uri', redirect_uri);
   
-    return this.http.post(apiURL,params, { headers })
+    return this.http.post<TokenResponse>(apiURL,params, { headers })
     
   }
 
