604292806869319ce4933abfd73ae39a04389cf5
Jriera <jordiriera81@gmail.com>
2021-12-08 15:25:50 +0100

    feat:Add calls for recent songs, liked songs and playlists

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -4,44 +4,84 @@ import { environment } from 'src/environments/environment.prod';
 import { SpotifyUser } from '../spotify-user';
 import * as CryptoJS from 'crypto-js';
 import { CookieService } from 'ngx-cookie-service';
+import { PlaylistResponse, TopTracksResponse, trackResponse } from '../responses.interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpotifyService {
   baseUrl = environment.baseUrl;
-  token:string = '';
-  constructor(private httpSpotify:HttpClient,
-              private cookieService:CookieService) { }
+  token: string = '';
+  constructor(
+    private httpSpotify: HttpClient,
+    private cookieService: CookieService
+  ) {}
 
-  getProfile(){
+  getProfile() {
     this.decodeToken();
     const headers = new HttpHeaders({
-      'Content-Type':'application/x-www-form-urlencoded',
-       'Authorization': `Bearer ${this.token}`
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Bearer ${this.token}`,
     });
-    return this.httpSpotify.get<SpotifyUser>(`${this.baseUrl}/me`,{headers})
+    return this.httpSpotify.get<SpotifyUser>(`${this.baseUrl}/me`, { headers });
   }
 
-  getRecentTracks(){
+  getRecentTracks() {
     this.decodeToken();
     const headers = new HttpHeaders({
-      'Content-Type':'application/x-www-form-urlencoded',
-       'Authorization': `Bearer ${this.token}`,
-       
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Bearer ${this.token}`,
     });
-    return this.httpSpotify.get(`https://api.spotify.com/v1/me/player/recently-played`,{headers})
+    return this.httpSpotify.get(
+      `https://api.spotify.com/v1/me/player/recently-played`,
+      { headers }
+    );
   }
 
-  decodeToken(){
+  getSavedTracks(offset?:string) {
+    this.decodeToken();
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    });
+    if(offset){
+      return this.httpSpotify.get<trackResponse>(
+        `https://api.spotify.com/v1/me/tracks?offset=${offset}`,
+        { headers }
+      );
+    }
+    return this.httpSpotify.get<trackResponse>(`https://api.spotify.com/v1/me/tracks`, {
+      headers,
+    });
+  }
 
-   const encoded =  this.cookieService.get('token');
-    const decoded = CryptoJS.AES.decrypt(encoded, 'secret key 123').toString(CryptoJS.enc.Utf8);
+  getPlaylists() {
+    this.decodeToken();
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    });
+    return this.httpSpotify.get<PlaylistResponse>(`https://api.spotify.com/v1/me/playlists`, {
+      headers,
+    });
+  }
+
+  getTop(type:string,time_range:string,limit?:number) {
+    this.decodeToken();
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    });
+    return this.httpSpotify.get<TopTracksResponse>(`https://api.spotify.com/v1/me/top/${type}?time_range=${time_range}`, {
+      headers,
+    });
+  }
+
+  decodeToken() {
+    const encoded = this.cookieService.get('token');
+    const decoded = CryptoJS.AES.decrypt(encoded, 'secret key 123').toString(
+      CryptoJS.enc.Utf8
+    );
     this.token = decoded;
   }
-
-
 }
-
-
-
