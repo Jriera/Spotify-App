8c3eb93069c25fb527b35f8361cf9af1c10c77dd
Jriera <jordiriera81@gmail.com>
2021-12-08 15:26:57 +0100

    feat:Add methods to display songs, playlists and top elements

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,10 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpService } from '../services/http.service';
 import { SpotifyService } from '../services/spotify.service';
 import { SpotifyUser } from '../spotify-user';
-import {TokenResponse} from '../token-response';
+import {TokenResponse} from '../responses.interfaces';
 import {finalize, map, pluck} from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 import * as CryptoJS from 'crypto-js';
+
 
 @Component({
   selector: 'app-welcome',
@@ -17,33 +18,11 @@ export class WelcomeComponent implements OnInit {
   code:string= '';
   TokenResponse:TokenResponse|null = null;
   accessToken:string='';
-  user:SpotifyUser={
-    country: '',
-    display_name: '',
-    email: '',
-    explicit_content: {
-      filter_enabled: false,
-      filter_locked: false
-    },
-    external_urls: {
-      spotify: ''
-    },
-    followers: {
-      href: '',
-      total: 0
-    },
-    href: '',
-    id: '',
-    images: [
-      {
-        height: 0,
-        url: '',
-        width: 0
-      }
-    ],
-    type: '',
-    uri: ''
-  };
+  user:SpotifyUser|null = null;
+  savedTotal: number=0;
+  playlistTotal:number=0;
+  mostPlayedSong='';
+  mostPlayedArtist='';
   constructor(private aRoute:ActivatedRoute,
               private http:HttpService,
               private spotify:SpotifyService,
@@ -53,7 +32,6 @@ export class WelcomeComponent implements OnInit {
     this.getCode();
     this.http.getToken(this.code).pipe(
       map(data=>{
-        /* this.spotify.token= data.access_token; */
         this.encode(data.access_token);
         
         const refreshToken = data.refresh_token;
@@ -63,6 +41,10 @@ export class WelcomeComponent implements OnInit {
         console.log('get token finalized');
         this.getUser();
         this.getRecent();
+        this.getSavedTotal();
+        this.getPlaylistTotal();
+        this.gettopTracks();
+        this.gettopArtists();
         this.getfreshToken(this.cookieService.get('refreshToken'));
         
       })).subscribe(data=>{
@@ -101,6 +83,31 @@ export class WelcomeComponent implements OnInit {
       });
     }
 
+    getSavedTotal(){
+      this.spotify.getSavedTracks().subscribe(data=>{
+        this.savedTotal = data.total;
+      });
+    }
+    getPlaylistTotal(){
+      this.spotify.getPlaylists().subscribe(data=>{
+        this.playlistTotal = data.total;
+      });
+    }
+
+    gettopTracks(){
+      this.spotify.getTop('tracks','long_term').subscribe(data=>{
+        this.mostPlayedSong = data.items[0].name;
+      });
+    }
+    gettopArtists(){
+      this.spotify.getTop('artists','long_term').subscribe(data=>{
+        this.mostPlayedArtist = data.items[0].name;
+      });
+    }
+
+    
+        
+      
     encode(value:string){
       const coded= CryptoJS.AES.encrypt(value, 'secret key 123').toString();
       this.cookieService.set('token', coded);
