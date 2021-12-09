56dd9019b37dc76cebe587688c45bb8c5bd0798d
Jriera <jordiriera81@gmail.com>
2021-12-09 01:13:17 +0100

    chore:register sidebar component to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import {HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { CookieService } from 'ngx-cookie-service';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 
@@ -15,6 +16,7 @@ import { CookieService } from 'ngx-cookie-service';
     AppComponent,
     WelcomeComponent,
     LoginComponent,
+    SidebarComponent,
     
   ],
   imports: [
